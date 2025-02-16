8c9471db221d47231780fdaed0f421125ef66ae9
Meszaros Nandor Farkas <farkas.nandor.meszaros@gmail.com>
2025-02-16 16:52:56 +0100

    fuggvenydokumentacio letrehozasa

--- main.js
+++ main.js
@@ -12,29 +12,29 @@ const fejlec  = //létrehozunk egy fejléc arrayt objektumokkal
     ]
 
 
-    const array = [
-        {
+    const array = [ //csinálunk egy arrayt 
+        { //elso objektum
             Nemzetiség : "Orosz",
             Szerző : "Gogol",
             Szerző1 : "Csehov",
             Mű1 : "	A köpönyeg",
             Mű2 : "A csinovnyik halála"
     
-        },
+        }, //masodik objektum
         {
             Nemzetiség : "Cseh",
             Szerző : "Franz Kafka",
             Mű1 : "	Az átváltozás",
     
-        },
+        }, //harmadik objektum
         {
             Nemzetiség : "Magyar",
             Szerző : "Örkény István	",
             Szerző1 : "József Attila	",
             Mű1 : "Egyperces Novellák",
             Mű2 : "Klárisok"
-    
-        },
+     
+        }, //negyedik objektum
         {
             Nemzetiség : "Svájc	",
             Szerző : "Friedrich Dürrenmatt	",
@@ -45,136 +45,147 @@ const fejlec  = //létrehozunk egy fejléc arrayt objektumokkal
     ]
 
 const formarray = [
-    {
+    { //elso obj
         label: "Származás:",
         id: "szarmazas",
         for: "szarmazas"
     },
-    {
+    { //masodik obj
         label: "1. szerző:",
         id: "szerzo1",
         for: "szerzo1"
     },
-    {
+    { //harmadik obj
         label: "1. szerző műve:",
         id: "szerzo1mu",
         for: "szerzo1mu"
     },
-    {
+    { //negyedik obj
         label: "2. szerző:",
         id: "szerzo2",
         for: "szerzo2"
     },
-    {
+    { //otodik objs
         label: "2. szerző műve:",
         id: "szerzo2mu",
         for: "szerzo2mu"
     }
 ]
-
-function formgenerate() {
-    const form = document.createElement('form')
-    form.id = "form"
+/**
+ * Ez generálja le nekünk a formot aminek a bemeneti értéke egy array. Azon iterálunk végig és csináljuk a formot.
+ * @param {array} array 
+ */
+function formgenerate(array) { //formgenerate function
+    const form = document.createElement('form') //létrehozunk egy formot
+    form.id = "form" //form idje legyen form
     form.action = "#"
 
-    for(let i = 0; i < formarray.length; i++) {
+    for(let i = 0; i < array.length; i++) { //végigiterálunk a formon
 
-        const div = document.createElement('div')
-        const label = document.createElement('label')
-        const input = document.createElement('input')
-        const br = document.createElement('br')
+        const div = document.createElement('div') //csinálnuk egy div-et
+        const label = document.createElement('label') //csinálunk egy label-t
+        const input = document.createElement('input') //csinálnuk egy input-ot
+        const br = document.createElement('br') //csinálunk egy br-t
 
-        label.innerHTML = formarray[i].label
-        label.htmlFor = formarray[i].for
-        input.type = "text"
-        input.id = formarray[i].id
-        input.name = formarray[i].id
+        label.innerHTML = formarray[i].label //a labelnek az innerhtmlje legyen az adott indexnél található label az arrayünkben
+        label.htmlFor = formarray[i].for//a labelnek az for-ja legyen az adott indexnél található for az arrayünkben
+        input.type = "text" //input typja legyen text
+        input.id = formarray[i].id //input idja legyen az adott id az arrayünkben
+        input.name = formarray[i].id  //input neve legyen az adott id az arrayünkben
 
-        const errordiv = document.createElement('div')
-        errordiv.className = "error"
+        const errordiv = document.createElement('div') //csinálunk egy errodivet
+        errordiv.className = "error" //ennek a divnek a classneve legyen error
     
 
-        div.appendChild(label)
-        div.appendChild(br)
-        div.appendChild(input)
-        div.appendChild(errordiv)
+        div.appendChild(label) //hozzátesszük a fődivhez a labelt
+        div.appendChild(br) //hozzátesszük a fődivhez a brt
+        div.appendChild(input) //hozzátesszük a fődivhez a inputot
+        div.appendChild(errordiv) //hozzátesszük a fődivhez az errordivet
 
-        form.appendChild(div)
+        form.appendChild(div) //az egész formhoz hozzátesszük a fődivet
     }
-    document.body.appendChild(form)
+    document.body.appendChild(form) //majd a formot a bodyhoz
     const button = document.createElement('button') //csinálunk egy gombot
     button.innerHTML = "Hozzáadás" //a gombnak legyen az innerhtml-je a "Hozzáadás"
     document.body.appendChild(form) //a bodyhoz hozzátesszük a formot
     form.appendChild(button) //majd a gombot pedig a formhoz
 }
 
-formgenerate()
+formgenerate(formarray)
 
-const tablazat = document.createElement('table')
+const tablazat = document.createElement('table') //csinálnuk egy tablet
 
-const tbody = document.createElement('tbody')
+const tbody = document.createElement('tbody')   //csinálnuk egy tbodyt
 
-const thead = document.createElement('thead')
+const thead = document.createElement('thead')  //csinálnuk egy theadet
 
-document.body.appendChild(tablazat)
-tablazat.appendChild(thead)
-tablazat.appendChild(tbody)
+document.body.appendChild(tablazat) //hozzátesszük a tablet a bodyhoz
+tablazat.appendChild(thead) //hozzátesszük a táblázathot  a theadet
+tablazat.appendChild(tbody)  //hozzátesszük a táblázathot  a tbpdyt
+ 
 
+const tr = document.createElement('tr') //csinálunk egy tr-t
+thead.appendChild(tr) //theadhez hozzátesszük a tr-t
 
-const tr = document.createElement('tr')
-thead.appendChild(tr)
-
-function headergenerator(headerobjek,sor) {
-    for(let adat of headerobjek) {
-        const th = document.createElement('th');
+/**
+ * Ez függvény generálja le nekünk a fejlécet bemeneti paraméterek alapján
+ * @param {array} headerobjek 
+ * @param {HTMLTableRowElement} sor 
+ */
+function headergenerator(headerobjek,sor) { 
+    for(let adat of headerobjek) { //végigmegyünk az adott array adatain
+        const th = document.createElement('th'); //csinálunk egy th-t
     
-        th.innerHTML = adat.szoveg
-        console.log(adat);
-        sor.appendChild(th);
-        if(adat.szoveg == "Mű") {
-            th.colSpan = 2
+        th.innerHTML = adat.szoveg //a th-nak a szövege legyen az adott arrayben lévő szöveg
+        sor.appendChild(th); //hozzátesszük a sorhoz amit beadtunk mint paraméter a th-t
+        if(adat.szoveg == "Mű") { //ha az adat szövege az "Mű"
+            th.colSpan = 2 //akkor az ottani Th legyen colspan2
         }
         }
     
 }
 
 headergenerator(fejlec,tr)
+/**
+ * Ez generálja le nekünk a táblaztot. Bemeneti értékei egy array és egy globális tbody.
+ * @param {array} thearray 
+ * @param {HTMLTableSectionElement} bodyelement 
+ */
+function rendertable(thearray, bodyelement) { 
+    for(let adat of thearray) { //végigmegyünk az adatokon az adott arrayben
+        const sor = document.createElement('tr') //csinálunk egy sort
 
-function rendertable(thearray, bodyelement) {
-    for(let adat of thearray) {
-        const sor = document.createElement('tr')
+        bodyelement.appendChild(sor) //az adott tbodyhoz hozzátesszük ezt a sort
 
-        bodyelement.appendChild(sor)
-
-        const elsocella = document.createElement('td')
-        elsocella.innerHTML = adat.Nemzetiség
-        if(adat.Szerző1 && adat.Mű1){
-            elsocella.rowSpan = 2
+        const elsocella = document.createElement('td') //csinálunk egy td
+        elsocella.innerHTML = adat.Nemzetiség //az elsocella innerhtml-je legyen az adott adat nemzetisége
+        if(adat.Szerző1 && adat.Mű1){ //ha az adott objektumban az arrayben van második szerző és mű
+            elsocella.rowSpan = 2 //akkor legyen a nemzetíség 2 rowspan
         }
-        sor.appendChild(elsocella)
+        sor.appendChild(elsocella) //hozzátesszük a sorunkhoz
 
-        const masidokcella = document.createElement('td')
-        masidokcella.innerHTML = adat.Szerző
-       
-        sor.appendChild(masidokcella)
-
-        const harmadikcella = document.createElement('td')
-        harmadikcella.innerHTML = adat.Mű1
-       
-        sor.appendChild(harmadikcella)
+        const masidokcella = document.createElement('td') //csinálink egy td-t
+        masidokcella.innerHTML = adat.Szerző //ennek legyen az innerhtml-je az adott szerző
         
-        if(elsocella.rowSpan == 2) {
-            const sor1 = document.createElement('tr')
+        sor.appendChild(masidokcella)//hozzátesszük a sorunkhoz
 
-            const negyedikcella = document.createElement('td')
-            negyedikcella.innerHTML = adat.Szerző1
+        const harmadikcella = document.createElement('td')  //csinálunk egy td-t
+        harmadikcella.innerHTML = adat.Mű1 //ennek legyen az innerhtml-je az adott mű
+       
+        sor.appendChild(harmadikcella)//hozzátesszük a sorunkhoz
+        
+        if(elsocella.rowSpan == 2) { //hogyha az elsocella rowspanja kettő azaz van tőbb szerző és mű
+            const sor1 = document.createElement('tr') //csinálunk egy új sort
+
+            const negyedikcella = document.createElement('td') //csinálunk egy tdt
+            negyedikcella.innerHTML = adat.Szerző1 //ennek legyen az értéke az adott szerző2 az arrayben
     
-            const ötödikcella = document.createElement('td')
-            ötödikcella.innerHTML = adat.Mű2
+            const ötödikcella = document.createElement('td') //csinálunk egy tdt
+            ötödikcella.innerHTML = adat.Mű2 //ennek legyen az értéke az adott mű2 az arrayben
     
-            sor1.appendChild(negyedikcella)
-            sor1.appendChild(ötödikcella)
-            bodyelement.appendChild(sor1)
+            sor1.appendChild(negyedikcella) //hozzátesszük a sorunkhoz
+            sor1.appendChild(ötödikcella) //hozzátesszük a sorunkhoz
+            bodyelement.appendChild(sor1) //és végűl a tbodyhoz hozzátesszük ezt az új sort
         }
        
 
@@ -209,25 +220,34 @@ form.addEventListener('submit', function(e) {
         errorelement.innerHTML = '' //annak legyen az innerhtml-je üres string. (igy eltűnik majd a validácios szöveg ha tényleg irunk valamit)
     }   
    
+    /**
+     * 
+     * @param {HTMLElement} ertek 
+     * @param {string} uzenet 
+     */
     
-    
-    function alapcheck(ertek, uzenet) {
-        if (!ertek.value) { 
-            valid = false
-            const parentElement = ertek.parentElement; 
-            const errormsg = parentElement.querySelector('.error'); 
+    function alapcheck(ertek, uzenet) { 
+        if (!ertek.value) {  //hogyha a bejövö értéknek a valuja undefined vagy ""
+            valid = false //akkor legyen valid false
+            const parentElement = ertek.parentElement;  //megnézzük a parentelementjét a bejövő HTMLELEMENT-nek
+            const errormsg = parentElement.querySelector('.error');  //megnézzük hogy van benne olyan element aminek a class-ja error.
             if (errormsg) { //ha az errormsg van akkor 
                 errormsg.innerHTML = uzenet; //legyen a megadott uzenetünk az
             }
         }
     
     }
-
+    /**
+     * Ez a check megnézi hogy ha az egyik érték az nem létezik de a másik igen akkor kiirjuk a megfelelő üzenetet az első értéknek.
+     * @param {HTMLElement} ertek 
+     * @param {HTMLElement} ertek2 
+     * @param {string} uzenet 
+     */
     function optionalcheck(ertek, ertek2, uzenet) {
-        if (!ertek.value && ertek2.value) { 
-            valid = false
-            const parentElement = ertek.parentElement; 
-            const errormsg = parentElement.querySelector('.error'); 
+        if (!ertek.value && ertek2.value) {  //hogyha az egyik bejővő érték undefined vagy "" és a másik pedig létezik
+            valid = false //akkor legyen valid false
+            const parentElement = ertek.parentElement;  //megnézzük a parentelementjét a bejövő HTMLELEMENT-nek
+            const errormsg = parentElement.querySelector('.error');   //megnézzük hogy van benne olyan element aminek a class-ja error.
             if (errormsg) { //ha az errormsg van akkor 
                 errormsg.innerHTML = uzenet; //legyen a megadott uzenetünk az
             }
@@ -236,13 +256,13 @@ form.addEventListener('submit', function(e) {
     }
     
     
-    alapcheck(szarmazas, "Szarmazas nem jó")
-    alapcheck(szerzo, "Szerzo nem jó")
-    alapcheck(szerzomu, "Mű nem jó nem jó")
+    alapcheck(szarmazas, "Szarmazas nem jó") //függvényhívás bemeneti értékkel
+    alapcheck(szerzo, "Szerzo nem jó")  //függvényhívás bemeneti értékkel
+    alapcheck(szerzomu, "Mű nem jó nem jó")  //függvényhívás bemeneti értékkel
 
 
-    optionalcheck(szerzo1, szerzo1mu, "Kérlek előszőr szerzőt adj meg!")
-    optionalcheck(szerzo1mu, szerzo1, "Ha megadtad a második szerzőt kérlek töltsd ki a második szerző művet.")
+    optionalcheck(szerzo1, szerzo1mu, "Kérlek előszőr szerzőt adj meg!")  //függvényhívás bemeneti értékkel
+    optionalcheck(szerzo1mu, szerzo1, "Ha megadtad a második szerzőt kérlek töltsd ki a második szerző művet.")  //függvényhívás bemeneti értékkel
 
 
 
